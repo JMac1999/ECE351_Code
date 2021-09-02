7fe99e973707a9a744570234de0793673837a181
JMac1999 <89801058+JMac1999@users.noreply.github.com>
2021-09-02 13:16:49 -0700

    Add files via upload

---
+++ McDaniel_Jared_ECE351_Lab0.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+################################################################
+# Jared McDaniel                                               #
+# ECE 351 Section 51                                           #
+# Lab 0                                                        #
+# 9/7/2021                                                     #
+# This code came from the Lab 0 pdf file.                      #
+#                                                              #                                                             
+################################################################
+
+import numpy
+import matplotlib.pyplot
+import scipy
+
---
+++ McDaniel_Jared_ECE351_Lab0_verify.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+################################################################
+# Jared McDaniel                                               #
+# ECE 351 Section 51                                           #
+# Lab 0                                                        #
+# 9/7/2021                                                     #
+# This code came from the Lab 0 pdf file.                      #
+#                                                              #                                                             
+################################################################
+
+import numpy
+import matplotlib.pyplot
+import scipy
+import control
+import pandas
